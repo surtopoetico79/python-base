c5141f18f06b863734ea0beaab0b4996cb0c2e50
marcelo morais <marcellomoraes36@gmail>
2023-08-08 17:16:06 -0300

    Implementei o script multi linguas

--- hello.py
+++ hello.py
@@ -1,2 +1,39 @@
 #!/usr/bin/env python3
-print ('Hello, World')
+"""Hello World Multi Linguas.
+
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem 
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Bruno Rocha"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+
+# Aqui aplicamos a condicional
+
+if current_language == "pt_BR":
+	msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+	msg = "Ciao, Mondo!"
+elif current_language == "fr_FR":
+	msg = "Bonjour, Monde!"
+
+print(msg)
