2571852e8ba42888c7eab83b933da329cf738aa8
marcelo morais <marcellomoraes36@gmail>
2023-08-10 17:24:35 -0300

    adicionado gitignore

--- hello.py
+++ hello.py
@@ -30,7 +30,7 @@ msg = "Hello, World!"
 # Aqui aplicamos a condicional
 
 if current_language == "pt_BR":
-	msg = "Olá, Mundo!"
+    msg = "Olá, Mundo!"
 elif current_language == "it_IT":
 	msg = "Ciao, Mondo!"
 elif current_language == "fr_FR":
